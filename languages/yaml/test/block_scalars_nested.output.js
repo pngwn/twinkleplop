0775727a0e545b726167417dae51b779171c9c71
pngwn <hello@pngwn.io>
2026-09-26 12:06:26 +0200

    end yaml block scalars at dedent

---
+++ languages/yaml/test/block_scalars_nested.output.js
@@ -0,0 +1,194 @@
+export const test = [
+	{
+		"type": "property",
+		"start": 0,
+		"end": 5,
+		"match": "steps"
+	},
+	{
+		"type": "punctuation",
+		"start": 5,
+		"end": 6,
+		"match": ":"
+	},
+	{
+		"type": "punctuation",
+		"start": 9,
+		"end": 10,
+		"match": "-"
+	},
+	{
+		"type": "property",
+		"start": 11,
+		"end": 14,
+		"match": "run"
+	},
+	{
+		"type": "punctuation",
+		"start": 14,
+		"end": 15,
+		"match": ":"
+	},
+	{
+		"type": "operator",
+		"start": 16,
+		"end": 17,
+		"match": "|"
+	},
+	{
+		"type": "string",
+		"start": 17,
+		"end": 50,
+		"match": "\n      echo hi\n\n      echo there\n"
+	},
+	{
+		"type": "punctuation",
+		"start": 52,
+		"end": 53,
+		"match": "-"
+	},
+	{
+		"type": "property",
+		"start": 54,
+		"end": 58,
+		"match": "name"
+	},
+	{
+		"type": "punctuation",
+		"start": 58,
+		"end": 59,
+		"match": ":"
+	},
+	{
+		"type": "identifier",
+		"start": 60,
+		"end": 64,
+		"match": "next"
+	},
+	{
+		"type": "comment",
+		"start": 65,
+		"end": 75,
+		"match": "# comment\n"
+	},
+	{
+		"type": "property",
+		"start": 79,
+		"end": 81,
+		"match": "if"
+	},
+	{
+		"type": "punctuation",
+		"start": 81,
+		"end": 82,
+		"match": ":"
+	},
+	{
+		"type": "boolean",
+		"start": 83,
+		"end": 87,
+		"match": "true"
+	},
+	{
+		"type": "punctuation",
+		"start": 90,
+		"end": 91,
+		"match": "-"
+	},
+	{
+		"type": "property",
+		"start": 92,
+		"end": 96,
+		"match": "with"
+	},
+	{
+		"type": "punctuation",
+		"start": 96,
+		"end": 97,
+		"match": ":"
+	},
+	{
+		"type": "property",
+		"start": 104,
+		"end": 110,
+		"match": "script"
+	},
+	{
+		"type": "punctuation",
+		"start": 110,
+		"end": 111,
+		"match": ":"
+	},
+	{
+		"type": "operator",
+		"start": 112,
+		"end": 114,
+		"match": ">-"
+	},
+	{
+		"type": "string",
+		"start": 114,
+		"end": 194,
+		"match": "\n        folded\n          more indented\n    \n        after whitespace-only line\n"
+	},
+	{
+		"type": "property",
+		"start": 198,
+		"end": 201,
+		"match": "env"
+	},
+	{
+		"type": "punctuation",
+		"start": 201,
+		"end": 202,
+		"match": ":"
+	},
+	{
+		"type": "identifier",
+		"start": 203,
+		"end": 204,
+		"match": "x"
+	},
+	{
+		"type": "property",
+		"start": 205,
+		"end": 208,
+		"match": "top"
+	},
+	{
+		"type": "punctuation",
+		"start": 208,
+		"end": 209,
+		"match": ":"
+	},
+	{
+		"type": "operator",
+		"start": 210,
+		"end": 211,
+		"match": "|"
+	},
+	{
+		"type": "string",
+		"start": 211,
+		"end": 219,
+		"match": "\n  body\n"
+	},
+	{
+		"type": "property",
+		"start": 219,
+		"end": 223,
+		"match": "done"
+	},
+	{
+		"type": "punctuation",
+		"start": 223,
+		"end": 224,
+		"match": ":"
+	},
+	{
+		"type": "number",
+		"start": 225,
+		"end": 226,
+		"match": "1"
+	}
+];
